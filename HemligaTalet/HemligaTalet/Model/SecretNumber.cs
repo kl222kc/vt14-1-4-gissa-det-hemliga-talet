e3565c7b60f0446151496340e7e1537fdad93be6
kl222kc <kl222kc@student.lnu.se>
2014-02-06 19:19:15 +0100

    Klassen påbörjad

---
+++ HemligaTalet/HemligaTalet/Model/SecretNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HemligaTalet.Model
+{
+
+    enum Outcome {Indefinite, Low, High, Correct, NoMoreGuesses, PreviousGuess};
+    public class SecretNumber
+    {
+        private int _number;
+        private List<int> _previousGuesses;
+        private const int _MaxNumberOfGuesses = 7;
+
+        public bool CanMakeGuess
+        {
+            get;
+        }
+
+        public int Count
+        {
+            get;
+        }
+
+        public int? Number
+        {
+            get;
+        }
+
+        public Outcome Outcome
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<int> PreviousGuesses
+        {
+            get;
+        }
+
+        public void Initalize()
+        {
+
+        }
+
+        public Outcome MakeGuess(int guess)
+        {
+            
+        }
+
+        public SecretNumber()
+        {
+
+        }
+    }
+}
